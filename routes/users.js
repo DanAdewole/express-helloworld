6b4fa546ce39ba61f92a174f91e24d6bdcb1c9a5
Daniel Adewole <adedaniel504@gmail.com>
2025-01-03 18:03:37 +0100

    add: express locallibrary tutorial

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool/', (req, res, next) => {
+  res.render("prac", { title: 'Cool Example', content: 'I can do whatever I want whenever I want to do it'});
+});
+
 module.exports = router;
